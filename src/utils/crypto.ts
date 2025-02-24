adce7d6ad0477dc07c05a096cfee7de31e2110a3
Dilusha Dasanayaka <dilusha.dasanayaka@aircanada.ca>
2025-02-24 14:32:29 +0530

    remove unwanted files

--- src/utils/crypto.ts
+++
@@ -1,5 +0,0 @@
-import CryptoJS from 'crypto-js';
-
-export const stringToMD5 = (input: string) => {
-  return CryptoJS.MD5(input).toString();
-};
--- src/utils/data-time.ts
+++
@@ -1,5 +0,0 @@
-// function to format timestamp into yyyy-mm-dd hh:mm:ss
-export function formatDateTime(timestamp: string): string {
-    const date = new Date(timestamp);
-    return date.toLocaleString();
-}
